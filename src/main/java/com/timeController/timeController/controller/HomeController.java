ee63312d4ab5d664c89fd9e87f53c61d07c84f04
Masud karim <msmasud578@gmail.com>
2021-03-21 16:00:37 -0800

    Rest endpoint for user info

--- src/main/java/com/timeController/timeController/controller/HomeController.java
+++ src/main/java/com/timeController/timeController/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.timeController.timeController.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.timeController.timeController.dao.UserRepository;
 import com.timeController.timeController.model.User;
 import com.timeController.timeController.service.UserRegisterService;
@@ -8,7 +11,9 @@ import com.timeController.timeController.service.UserRegisterService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,18 +34,40 @@ public class HomeController {
 		
 	}
 	@GetMapping(value = "/user")
-	public User getUserList() {
-		return null;
+	public List<User> getUserList() {
+		List<User> userList = userRepo.findAll();
+		return userList;
 	}
 
-	@PostMapping(value = "/user")
-	public void registerUser(@RequestBody User userPojo) {
+	@PostMapping(value = "/user",consumes={MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE},produces={MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
+	public User registerUser(@RequestBody User userPojo) {
 		// This route is responsible for register the user 
 		logger.debug("registering the user data as : ");
 		logger.debug("First name : "+userPojo.getFirst_name()+"\nSecond name : "+userPojo.getSecond_name());
 		logger.debug("Email :"+userPojo.getEmail());
 
+		User findUser = userRepo.findByEmail(userPojo.getEmail());
+
+		if (findUser.getEmail().equals(userPojo.getEmail())) {
+			System.out.println("Address is already there.. You have to use somethign else");
+			User error  = new User();
+			error.setEmail(null);
+			error.setPassword(null);
+			error.setFirst_name(null);
+			error.setSecond_name("Email Already exist");
+			return error;
+		}
+
+		
+
 		regUser.register(userPojo);
-		// Th
+		return userPojo;
+	}
+	@GetMapping(value = "/user/{:id}")
+	public User getUserById(@PathVariable Long id) {
+		User user = userRepo.getOne(id);
+		return user;
+
+
 	}
 }
